a4eef9a7660524fc0a2fb98e3a410f1c0d5f0e5c
Stroller15 <shubhamverma6351@gmail.com>
2023-12-27 17:00:43 +0530

    fix supabase domain like error in next config file

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
       serverActions: true,
     },
     images: {
-      domains: ['fmliqecnrxgzwvucipeu.supabase.co'],
+      domains: ['wmtadllewbodozucjlpt.supabase.co'],
     },
   };
   
